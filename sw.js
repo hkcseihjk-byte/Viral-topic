f60859c92682b57406e94356b9934db8fcf04c23
hkcseihjk-byte <hkcseihjk@gmail.com>
2026-02-25 19:52:58 +0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-self.options = optp
-    "domain": "5gvci.com",
-    "zoneId": 10644590
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10644956
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
